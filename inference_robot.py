e18fbe18296abd520793165c045095ca349382f3
Plaif_vision_park <chansoo0710@gmail.com>
2022-04-04 18:32:25 +0900

    Update: RealSense and MindVision mode

--- inference_robot.py
+++ inference_robot.py
@@ -11,7 +11,7 @@ from utils.pyrealsense_camera import RealSenseCamera
 import rospy
 # ros numpy install 방법 
 # sudo apt-get install ros-$release-ros-numpy ($release는 현재 사용하는 ROS version (ex: melodic))
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, String
 import message_filters
 from sensor_msgs.msg import Image, CameraInfo
 import json
@@ -19,130 +19,117 @@ from utils.CameraManager import CameraManager
 
 tf.keras.backend.clear_session()
 bridge = CvBridge()
+
 rospy.init_node('topic_publisher', anonymous=True)
-pub = rospy.Publisher('counter', Float32, queue_size=1)
+pub = rospy.Publisher('counter', String, queue_size=1)
 rate = rospy.Rate(60)
+
 with open('./vision_grasp.json', 'r') as f_config:
     config_data = json.load(f_config)
-
+        
+camera = 0
 camera_config_data = config_data["cameras"]
-cameras = []
 for idx, cam in enumerate(camera_config_data):
-    
     topic_info = cam["topics"]
     camera = CameraManager({'name':cam["name"],'cameraInfos':topic_info["info"],'colorStream':topic_info["color"],'depthStream':topic_info["depth"],'calibration':cam["calibration"]})
-    
-
 camera.register_cb()
+
+def load_realSense():
+    # Load RealSense Camera
+    with open('./vision_grasp.json', 'r') as f_config:
+        config_data = json.load(f_config)
+
+    camera_config_data = config_data["cameras"]
+    for idx, cam in enumerate(camera_config_data):
+        topic_info = cam["topics"]
+        camera = CameraManager({'name':cam["name"],'cameraInfos':topic_info["info"],'colorStream':topic_info["color"],'depthStream':topic_info["depth"],'calibration':cam["calibration"]})
+    camera.register_cb()
+
+    return camera
+    
+def load_mindVision():
+    def jh_img_callback(img):
+        global bgr
+        rospy.get_rostime().to_sec()
+        bgr = bridge.imgmsg_to_cv2(img, desired_encoding='bgr8')
+
+    color_sub = message_filters.Subscriber('mv_cam', Image) 
+    color_sub.registerCallback(jh_img_callback)
+
+    return bgr
+
 result_roi_pub = rospy.Publisher('ROI_semantic_segmentation_result', Image, queue_size=1)
 result_seg_pub = rospy.Publisher('Segmentation_result', Image, queue_size=1)
 result_final_pub = rospy.Publisher('Final_result', Image, queue_size=1)
 
+TEST_IMG = rospy.Publisher('JH_IMG_TEST', Image, queue_size=1)
+# TEST_IMG.publish(img)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size",     type=int,   help="배치 사이즈값 설정", default=1)
-parser.add_argument("--model_name",     type=str,   help="저장될 모델 이름",
-                    default=str(time.strftime('%m%d', time.localtime(time.time()))))
-parser.add_argument("--checkpoint_dir", type=str,   help="모델 저장 디렉토리 설정", default='./checkpoints/')
-
+parser.add_argument("--camera_mode",     type=int,   help="Camera Mode || 1 : RealSense launch  2 : MindVision", default=1)
 args = parser.parse_args()
-BATCH_SIZE = args.batch_size
-CHECKPOINT_DIR = args.checkpoint_dir
+CAM_MODE = args.camera_mode
 IMAGE_SIZE = (480, 640)
-# IMAGE_SIZE = (720, 1280)
-# IMAGE_SIZE = (128, 128)
+ROI_SIZE = (128, 128)
 
-def interlace(imgL, imgR, h, w):
-    inter = np.empty((h, w, 3), imgL.dtype)
-    inter[:h:2, :w, :] = imgL[:h:2, :w, :]
-    inter[1:h:2, :w, :] = imgR[1:h:2, :w, :]
-    return inter
-
-def sharpning(img):
-    sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    img = cv2.filter2D(img, -1, sharpen_kernel)
-    return img
-
-def predict_full_image(img):
-    img = tf.cast(rgb, dtype=tf.float32)
-    img = preprocess_input(img, mode='torch')
-    img = tf.expand_dims(img, axis=0)
-
-    pred = model.predict_on_batch(img)
-    pred = tf.argmax(pred[0], axis=-1)
-
-    # Calculate x,y coordinates
-    yx_coords = np.mean(np.column_stack(np.where(pred == 2)),axis=0)
-
-    # For visualization
-    output = pred.numpy() * 127
-    output = output.astype(np.uint8)
-    
-    rgb =rgb.astype(np.uint8)
-    rgb = cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-    
-    if np.isnan(yx_coords[0]) != True:
-        cv2.circle(rgb, (int(yx_coords[1]), int(yx_coords[0])), int(3), (0, 0, 255), 3, cv2.LINE_AA)
-
-    output = cv2.cvtColor(output,cv2.COLOR_GRAY2RGB)
-
-    return output, yx_coords
-
-def predict_roi_image(img):
-    return img
-
-def crop_center(img,cropx,cropy):
-    y,x = img.shape
-    startx = x//2-(cropx//2)
-    starty = y//2-(cropy//2)    
-    return img[starty:starty+cropy,startx:startx+cropx]
-
-
-if __name__ == '__main__':  
+if __name__ == '__main__':
+    # Segmentation 모델 불러오기
     model = segmentation_model(image_size=IMAGE_SIZE)
     roi_model = semantic_model(image_size=(128, 128))
 
+    # Segmentation 모델 가중치 불러오기
     weight_name = '_0323_L-bce_B-16_E-100_Optim-Adam_best_iou'
     roi_weight_name = '_0330_roi-CE-B16-E100-C16-SWISH-ADAM_best_iou'
     model.load_weights(weight_name + '.h5')
     roi_model.load_weights(roi_weight_name + '.h5')
-    
-    img = tf.zeros([480, 640, 3])
-    img = tf.image.resize(img, size=IMAGE_SIZE,
-                    method=tf.image.ResizeMethod.BILINEAR)   
+
+    # Warm-Up Deep Learning Model's (한 번 Inference하여 대기 시간을 줄임) 
+    img = tf.zeros([IMAGE_SIZE[0], IMAGE_SIZE[1], 3])
     img = tf.cast(img, dtype=tf.float32)
     img = preprocess_input(img, mode='torch')
     img = tf.expand_dims(img, axis=0)
     pred = model.predict_on_batch(img)
 
-
-    roi_img = tf.zeros([128, 128, 3])
+    roi_img = tf.zeros([ROI_SIZE[0], ROI_SIZE[1], 3])
     roi_img = tf.cast(roi_img, dtype=tf.float32)
     roi_img = preprocess_input(roi_img, mode='torch')
     roi_img = tf.expand_dims(roi_img, axis=0)
     roi_pred = roi_model.predict_on_batch(roi_img)
 
-
-    # 944622074360 D435i
-    # f1181780 park pc l515
-    # cam = RealSenseCamera(device_id='f0350818', width=IMAGE_SIZE[1], height=IMAGE_SIZE[0], fps=30) #0003b661b825 # f0350818 # f1181780 # f1231507 # 944622074360
-    # cam.connect() 
-
+    # RosTopic으로 publish할 변수 (예측한 홀의 중심 x,y 좌표)
     previous_yx = [0, 0]
-    while True:
-        x,y,w,h = 0, 0, 0, 0
 
-        # image_bundle = cam.get_image_bundle()
-        # rgb  = image_bundle['rgb']
-        rgb = camera.color.copy()[120:120+480, 320:320+640]
+    # Load Camera
+    if CAM_MODE == 1:
+        # Load RealSense Camera
+        # camera = load_realSense()
+        # Get Camera RGB shape (h, w, c)
+        rgb_shape = camera.color.shape
+        y_index = (rgb_shape[0] - IMAGE_SIZE[0]) // 2 
+        x_index = (rgb_shape[1] - IMAGE_SIZE[1]) // 2
+
+
+
+    elif CAM_MODE == 2:
+        # Load MindVision Camera
+        original = load_mindVision()
+
+        # Get Camera RGB shape (h, w, c)
+        rgb_shape = original.copy().shape
+        y_index = (rgb_shape[0] - IMAGE_SIZE[0]) // 2 
+        x_index = (rgb_shape[1] - IMAGE_SIZE[1]) // 2
+
+
+    # Set x,y,w,h Variable Initialization
+    x,y,w,h = 0, 0, 0, 0
+
+    # Main Loop
+    while True:
+        original = camera.color.copy()
+        rgb = original[y_index:y_index+IMAGE_SIZE[0], x_index:x_index+IMAGE_SIZE[1]]
         
-        
-        
-        # Inference
-        img = tf.image.resize(rgb, size=IMAGE_SIZE,
-                        method=tf.image.ResizeMethod.BILINEAR)
-                                        
-        img = tf.cast(img, dtype=tf.float32)
+        # Inference                     
+        img = tf.cast(rgb, dtype=tf.float32)
         img = preprocess_input(img, mode='torch')
         img = tf.expand_dims(img, axis=0)
         pred = model.predict_on_batch(img)
@@ -175,7 +162,7 @@ if __name__ == '__main__':
 
             if x != 0:
                 cropped_input_img = rgb.copy()[y:y+h, x:x+w]
-                ROI = tf.image.resize_with_crop_or_pad(cropped_input_img, 128, 128)
+                ROI = tf.image.resize_with_crop_or_pad(cropped_input_img, ROI_SIZE[0], ROI_SIZE[1])
                 ROI = tf.cast(ROI, dtype=tf.float32)
                 ROI = preprocess_input(ROI, mode='torch')
                 ROI = tf.expand_dims(ROI, axis=0)
@@ -192,10 +179,12 @@ if __name__ == '__main__':
                 zero_img = np.zeros((rgb.shape[0], rgb.shape[1]))
 
                 ROI_PRED = tf.where(ROI_PRED==2, 127, 0)
+                ROI_PRED = ROI_PRED.numpy()
 
-
-                ROI_PRED = crop_center(ROI_PRED.numpy(), w, h)
-
+                crop_y, crop_x = ROI_PRED.shape
+                startx = crop_x // 2 - (w // 2) # x
+                starty = crop_y // 2 - (h // 2) # y   
+                ROI_PRED = ROI_PRED[starty:starty+h, startx:startx+w]
                             
                 if 512+64 >= x >= 128-64:
                     if 352+64 >= y >= 128-64:
@@ -217,20 +206,12 @@ if __name__ == '__main__':
             
                         if np.isnan(yx_coords[0]) != True:
                             previous_yx = yx_coords
-                            cv2.circle(rgb, (int(yx_coords[1]), int(yx_coords[0])), int(3), (0, 0, 255), 3, cv2.LINE_AA)
-        
-        # rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
-        # cv2.imshow('Output', rgb)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        
-        img_msg = bridge.cv2_to_imgmsg(rgb, encoding='bgr8')
+                            # cv2.circle(rgb, (int(yx_coords[1]), int(yx_coords[0])), int(3), (0, 0, 255), 3, cv2.LINE_AA)
+                            original[y_index:y_index+IMAGE_SIZE[0], x_index:x_index+IMAGE_SIZE[1]] = rgb
+                            cv2.circle(original, (int(previous_yx[1] + x_index), int(previous_yx[0] + y_index)), int(3), (0, 0, 255), 3, cv2.LINE_AA)
+            
+        img_msg = bridge.cv2_to_imgmsg(original, encoding='bgr8')
         result_final_pub.publish(img_msg)
-        
-        # print('x :', previous_yx[1], 'y :', previous_yx[0])
-        
-        
-        
 
-        pub.publish(previous_yx[0])
+        pub.publish(f'x : {previous_yx[1]} , y : {previous_yx[0]}')
        
