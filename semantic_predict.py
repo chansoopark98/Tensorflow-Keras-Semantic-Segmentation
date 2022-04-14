de93910dc82d6d9b502e09a07a2a9748356bfadf
Plaif_vision_park <chansoo0710@gmail.com>
2022-04-14 12:04:13 +0900

    Code refact

--- semantic_predict.py
+++ semantic_predict.py
@@ -10,13 +10,18 @@ import matplotlib.pyplot as plt
 tf.keras.backend.clear_session()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size",     type=int,   help="배치 사이즈값 설정", default=1)
+parser.add_argument("--batch_size",     type=int,
+                    help="배치 사이즈값 설정", default=1)
 parser.add_argument("--model_name",     type=str,   help="저장될 모델 이름",
                     default=str(time.strftime('%m%d', time.localtime(time.time()))))
-parser.add_argument("--dataset_dir",    type=str,   help="데이터셋 다운로드 디렉토리 설정", default='./datasets/')
-parser.add_argument("--result_dir", type=str,   help="Test result dir", default='./results/')
-parser.add_argument("--checkpoint_dir", type=str,   help="모델 저장 디렉토리 설정", default='./checkpoints/')
-parser.add_argument("--weight_name", type=str,   help="모델 가중치 이름", default='weight_name.h5')
+parser.add_argument("--dataset_dir",    type=str,
+                    help="데이터셋 다운로드 디렉토리 설정", default='./datasets/')
+parser.add_argument("--result_dir", type=str,
+                    help="Test result dir", default='./results/')
+parser.add_argument("--checkpoint_dir", type=str,
+                    help="모델 저장 디렉토리 설정", default='./checkpoints/')
+parser.add_argument("--weight_name", type=str,
+                    help="모델 가중치 이름", default='weight_name.h5')
 
 args = parser.parse_args()
 BATCH_SIZE = args.batch_size
@@ -35,7 +40,8 @@ os.makedirs(MASK_RESULT_DIR, exist_ok=True)
 TRAIN_INPUT_IMAGE_SIZE = IMAGE_SIZE
 VALID_INPUT_IMAGE_SIZE = IMAGE_SIZE
 
-test_dataset_config = SemanticGenerator(DATASET_DIR, TRAIN_INPUT_IMAGE_SIZE, BATCH_SIZE, mode='validation')
+test_dataset_config = SemanticGenerator(
+    DATASET_DIR, TRAIN_INPUT_IMAGE_SIZE, BATCH_SIZE, mode='validation')
 test_set = test_dataset_config.get_testData(test_dataset_config.valid_data)
 test_steps = test_dataset_config.number_valid // BATCH_SIZE
 
@@ -64,7 +70,7 @@ for x, y, original in tqdm(test_set, total=test_steps):
     ax0.axis("off")
 
     ax0 = fig.add_subplot(rows, cols, 2)
-    ax0.imshow(label) 
+    ax0.imshow(label)
     ax0.set_title('label')
     ax0.axis("off")
 
@@ -73,8 +79,5 @@ for x, y, original in tqdm(test_set, total=test_steps):
     ax1.set_title('pred')
     ax1.axis("off")
 
-
     batch_idx += 1
     plt.savefig(RESULT_DIR + str(batch_idx) + '_output.png', dpi=300)
-
-
